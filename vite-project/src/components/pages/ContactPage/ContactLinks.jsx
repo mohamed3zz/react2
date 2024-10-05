68be96eef3501d475cf181b3c90ceeda5f502dbb
mohamed3zz <elgendymohamed7y@gmail.com>
2024-10-05 18:08:38 +0300

    first commit

---
+++ vite-project/src/components/pages/ContactPage/ContactLinks.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export default function ContactLinks() {
+  return (
+    <div>
+     <div className='contact_links'>
+         <NavLink  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "activev" : ""
+  } to={""}>Contact Us Form</NavLink>
+        <NavLink to={'emails'}>Emails</NavLink>
+       <NavLink to={'locations'}>Locations</NavLink>
+       <NavLink to={'phonenumbers'}>Phone Numbers</NavLink>
+     </div>
+     
+    </div>
+  )
+}
